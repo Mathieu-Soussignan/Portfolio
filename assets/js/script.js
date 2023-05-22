bcf306f37981494b4328ae7a34e0f09b483d844e
MSO <mathieu.soussignan@hotmail.fr>
2023-05-22 12:51:02 +0200

    [Update] Modification fichiers script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["développeur frontend ", "développeur backend ", "développeur web"],
+    strings: ["développeur web ", "développeur web mobile ", "développeur fullstack"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
